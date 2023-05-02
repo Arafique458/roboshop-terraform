62fa2499a325c0f47cb52c6bed5df9851bfe74c9
darkroom <arafique458@gmail.com>
2023-05-01 21:55:16 -0400

    Added for each loops

--- servers.tf
+++ servers.tf
@@ -4,201 +4,74 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
 }
 
-#declaring a variable
-variable "instance_type" {
-  default = "t3.small"
-}
-
 # declaring security groups to join too.
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
-# Frontend EC2 instance
-resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-  tags = {
-    Name = "frontend"
+# for_each loop
+variable "components" {
+  frontend = {
+    name          = "frontend"
+    instance_type = "t3.small"
+  }
+  mongodb = {
+    name          = "mongodb"
+    instance_type = "t3.small"
+  }
+  catalogue = {
+    name          = "catalogue"
+    instance_type = "t3.small"
+  }
+  cart = {
+    name          = "cart"
+    instance_type = "t3.small"
+  }
+  shipping = {
+    name          = "shipping"
+    instance_type = "t3.medium"
+  }
+  mysql = {
+    name          = "mysql"
+    instance_type = "t3.small"
+  }
+  redis = {
+    name          = "redis"
+    instance_type = "t3.small"
+  }
+  user = {
+    name          = "user"
+    instance_type = "t3.small"
+  }
+  payment = {
+    name          = "payment"
+    instance_type = "t3.small"
+  }
+  rabbitmq = {
+    name          = "rabbitmq"
+    instance_type = "t3.small"
   }
 }
-## DNS Record Frontend
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "frontend.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
 
-# mongodb EC2 instance
-resource "aws_instance" "mongodb" {
+
+
+# All the instance creation
+resource "aws_instance" "instance" {
+  for_each      = var.components
   ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "mongodb"
+    Name = each.value["name"]
   }
 }
-## DNS Record mongodb
-resource "aws_route53_record" "mongodb" {
+## DNS Records
+resource "aws_route53_record" "records" {
+  for_each = var.components
   zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "mongodb.devopsdude.cloud"
+  name    = "${each.value["name"]}.devopsdude.cloud"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-# catalogue EC2 instance
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-## DNS Record catalogue
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "catalogue.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-# redis EC2 instance
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "redis"
-  }
-}
-## DNS Record redis
-resource "aws_route53_record" "redis" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "redis.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-# user EC2 instance
-resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "user"
-  }
-}
-## DNS Record user
-resource "aws_route53_record" "user" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "user.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-# shipping EC2 instance
-resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "shipping"
-  }
-}
-## DNS Record shipping
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "shipping.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-# mysql EC2 instance
-resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-## DNS Record mysql
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "mysql.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-# rabbitmq EC2 instance
-resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-## DNS Record rabbitmq
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "rabbitmq.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-# cart EC2 instance
-resource "aws_instance" "cart" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "cart"
-  }
-}
-## DNS Record cart
-resource "aws_route53_record" "cart" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "cart.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-# payment EC2 instance
-resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "payment"
-  }
-}
-## DNS Record payment
-resource "aws_route53_record" "payment" {
-  zone_id = "Z08715033JYQ7WM1QMZBS"
-  name    = "payment.devopsdude.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
+  records = [aws_instance.instance[each.value["name"]].private_ip]
 }
