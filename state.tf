524253568cda31d4146db2254339e524e9575608
darkroom <arafique458@gmail.com>
2023-05-02 20:43:55 -0400

    Created : s3 bucket folder

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-b072"
+    bucket = "terraform-b072" # name must be unique
     key    = "roboshop/dev/terraform.tfstate"
     region = "us-east-1"
   }
