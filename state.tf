f882806f7e09ba150ee05f37f1988920df82b8c0
arafique458 <arafique458@gmail.com>
2023-05-04 18:08:07 -0400

    Keeping the code dry Created: env-dev ---->state.tfvars env-pro ---->state.tfvars
    in the state.tf we made the variable to accomplish DRY coding techniques.

--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
-//terraform {
- // backend "s3" {
- //   bucket = "terraform-b072" # name must be unique
-   // key    = "roboshop/dev/terraform.tfstate"
-  //  region = "us-east-1"
- // }
-//}
+terraform {
+  backend "s3" {}
+}
