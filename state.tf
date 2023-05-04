725259188184f2efb3edd322b5597ece735d4412
arafique458 <arafique458@gmail.com>
2023-05-04 00:11:01 -0400

    Commneted out the state.tf

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-b072" # name must be unique
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+//terraform {
+ // backend "s3" {
+ //   bucket = "terraform-b072" # name must be unique
+   // key    = "roboshop/dev/terraform.tfstate"
+  //  region = "us-east-1"
+ // }
+//}
