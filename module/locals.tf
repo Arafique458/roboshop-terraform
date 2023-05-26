ed6ba583c86e908b251ab50640d34f247ae0bd35
arafique458 <arafique458@gmail.com>
2023-05-25 20:52:52 -0400

    Creating IAM role Terraform

--- module/locals.tf
+++ module/locals.tf
@@ -8,7 +8,8 @@ locals {
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
-    "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/Arafique458/roboshop-ansbile.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+#    "sudo labauto ansible",
+#    "ansible-pull -i localhost, -U https://github.com/Arafique458/roboshop-ansbile.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+  "echo OK"
   ]
 }
