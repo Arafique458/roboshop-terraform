ad88b32ce793cf3a9d2b4efbac37b2faa1f9d6de
arafique458 <arafique458@gmail.com>
2023-05-25 21:37:18 -0400

    Instance Profile AWS_IAM_instance

--- module/locals.tf
+++ module/locals.tf
@@ -8,8 +8,7 @@ locals {
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
-#    "sudo labauto ansible",
-#    "ansible-pull -i localhost, -U https://github.com/Arafique458/roboshop-ansbile.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
-  "echo OK"
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/Arafique458/roboshop-ansbile.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
