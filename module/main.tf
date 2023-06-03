3c68e2ebc2af791c76839399ffbbacb479b73317
arafique458 <arafique458@gmail.com>
2023-06-03 16:48:35 -0400

    Instance Profile AWS_IAM_instance

--- module/main.tf
+++ module/main.tf
@@ -54,14 +54,13 @@ resource "aws_iam_role" "role" {
   }
 }
 
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.component_name}-${var.env}-role"
-  role = aws_iam_role.role.name
-}
+#resource "aws_iam_instance_profile" "instance_profile" {
+#  name = "${var.component_name}-${var.env}-role"
+#  role = aws_iam_role.role.name
+#}
 
-resource "aws_iam_role_policy" "ssm-ps-policy" {
+resource "aws_iam_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.role.id
 
   policy = jsonencode({
     "Version" : "2012-10-17",
